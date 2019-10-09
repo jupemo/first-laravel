a31a8c8e356a6e79346d2721b99087c494e690bd
Juan Pedro <otojupe@gmail.com>
2019-10-09 19:04:54 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,22 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/login', function () {
+    return view('auth.login');
+});
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+Route::get('/catalog', function () {
+    return view('catalog.index');
+});
+Route::get('/catalog/show/{id}', function () {
+    return view('catalog.show');    
+});
+Route::get('/catalog/edit/{id}', function () {
+    return view('catalog.edit');    
+});
+
+
